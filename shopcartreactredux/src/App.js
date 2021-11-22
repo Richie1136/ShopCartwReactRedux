b2613090ff441b975592da9887edae9f4c9792db
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-22 16:40:10 -0500

    Sending my cart data to firebase

--- shopcartreactredux/src/App.js
+++ shopcartreactredux/src/App.js
@@ -2,11 +2,20 @@ import Cart from './components/cart/Cart';
 import Layout from './components/layout/Layout';
 import Products from './components/products/Products';
 import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
 
 
 function App() {
 
   const isUi = useSelector(state => state.ui.cartVisible)
+
+  const cart = useSelector(state => state.cart)
+
+  useEffect(() => {
+    fetch('https://react-foodapp-ac153-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT'
+    })
+  }, [cart])
   return (
     <Layout>
       {isUi && <Cart />}
--- shopcartreactredux/src/store/cart-slice.js
+++ shopcartreactredux/src/store/cart-slice.js
@@ -4,6 +4,10 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState: { items: [], totalQuantity: 0 },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const Newitem = action.payload
       const existingItem = state.items.find(item => item.id === Newitem.id)
