cd51eb7e541bca784a5254a5f3fd425ed6d2df2f
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-11-23 09:05:21 -0500

    Added a notification component which will display a different message based on the status and then added a notification slice to my UI slice which has a object with a status, a message and a title and then inside of app.js i dispatched different actions

--- shopcartreactredux/src/App.js
+++ shopcartreactredux/src/App.js
@@ -1,21 +1,39 @@
 import Cart from './components/cart/Cart';
 import Layout from './components/layout/Layout';
 import Products from './components/products/Products';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react'
+import { uiActions } from './store/ui-slice'
 
 
 function App() {
-
+  const dispatch = useDispatch()
   const isUi = useSelector(state => state.ui.cartVisible)
 
   const cart = useSelector(state => state.cart)
 
   useEffect(() => {
-    fetch('https://react-foodapp-ac153-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart)
-    })
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending Data'
+      }))
+      const response = await fetch('https://react-foodapp-ac153-default-rtdb.firebaseio.com/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart)
+      })
+
+      if (!response.ok) {
+        throw new Error("Sending data failed")
+      }
+      const data = await response.json()
+      dispatch(uiActions.showNotification({
+        status: 'Success',
+        title: 'Successful',
+        message: 'Data has been sent successfully'
+      }))
+    }
   }, [cart])
   return (
     <Layout>
--- shopcartreactredux/src/store/ui-slice.js
+++ shopcartreactredux/src/store/ui-slice.js
@@ -2,11 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: 'ui',
-  initialState: { cartVisible: false },
+  initialState: { cartVisible: false, notification: null },
   reducers: {
     toggle(state) {
       state.cartVisible = !state.cartVisible
-    }
+    },
+    showNotification(state, action) {
+      state.notification = { status: action.payload.status, title: action.payload.title, message: action.payload.message }
+    },
   }
 })
 
